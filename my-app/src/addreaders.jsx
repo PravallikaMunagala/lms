9e8c5be906f9857eb3c3be9ba68f61e7e611326c
PravallikaMunagala <93988191+PravallikaMunagala@users.noreply.github.com>
2021-11-27 13:46:59 +0530

    Add files via upload

---
+++ my-app/src/addreaders.jsx
@@ -0,0 +1,116 @@
+import axios from "axios";
+import React, { Component } from 'react';
+
+class AddReaders extends React.Component {
+    state = {
+        readers: {
+            firstName: "",
+            lastName: "",
+            password: "",
+            mobileno: "",
+            email: "",
+        },
+    };
+
+    handleChange = (event) => {
+        const readers = { ...this.state.readers };
+        readers[event.target.name] = event.target.value;
+        console.log(event.target.name);
+        console.log(event.target.value);
+        this.setState({ readers: readers });
+    };
+    handleSubmit = (event) => {
+        event.preventdefault();
+        console.log("handleSubmit");
+        axios
+            .post("http://localhost:8080/lms/register", this.state.readers)
+            .then((response) => {
+                this.props.history.push("/readers");
+            })
+            .catch((error) => console.log(error));
+    };
+
+
+    render() {
+        return (
+
+            <div>
+                <h1>Readers Form </h1>
+                <form onSubmit={this.handleSubmit} className="w-50 mx-auto border p-3">
+                    <div className="mb-3">
+                        <label for="exampleInputFirstName" className="form-label">
+                            FirstName
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputFirstName"
+                            aria-describedby="emailHelp"
+                            value={this.state.readers.firstName}
+                            name="firstName"
+                            onChange={this.handleChange}
+                        />
+
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputLastName" className="form-label">
+                            LastName
+                        </label>
+                        <input type="text"
+                            className="form-control"
+                            id="exampleInputLastName"
+                            value={this.state.readers.lastName}
+                            name="lastName"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">
+                            Password
+                        </label>
+                        <input type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            value={this.state.readers.password}
+                            name="password"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputMobileNo" className="form-label">
+                            MobileNo
+                        </label>
+                        <input type="tel"
+                            className="form-control"
+                            id="exampleInputMobileNo"
+                            value={this.state.readers.mobileno}
+                            name="mobileno"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            value={this.state.readers.email}
+                            name="email"
+                            onChange={this.handleChange}
+                        />
+                    </ div>
+                    <div className="d-grid gap-2">
+                        <input type="submit" className="btn btn-primary" />
+
+                    </ div>
+                </form>
+
+            </div>
+        );
+
+    }
+}
+
+export default AddReaders;
